07cd70722885eaecc9a7ea4c4ee4596aa9b6990c
mcrymbo <croco2023@gmail.com>
2024-04-11 10:07:13 +0300

    connect to database

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,7 +5,10 @@ A module that returns the log message obfuscated
 import logging
 from typing import List
 import re
+from os import environ
+from mysql.connector import connection
 
+PII_FIELDS = ('name', 'email', 'password', 'ssn', 'phone')
 
 def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     """ filter data """
@@ -32,3 +35,27 @@ class RedactingFormatter(logging.Formatter):
         """ filters incoming log record """
         return filter_datum(self.fields, self.REDACTION, super(
             RedactingFormatter, self).format(record), self.SEPARATOR)
+
+def get_logger() -> logging.Logger:
+    """ logs user data """
+    logger = logging.getLogger('user_data')
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(stream_handler)
+    return logger
+
+def get_db() -> connection.MySQLConnection:
+    """ connect to mysql server """
+    username = environ.get("PERSONAL_DATA_DB_USERNAME")
+    password = environ.get("PERSONAL_DATA_DB_PASSWORD")
+    host = environ.get("PERSONAL_DATA_DB_HOST")
+    db = environ.get("PERSONAL_DATA_DB_NAME")
+
+    return connection.MySQLConnection(
+            user=username,
+            password=password,
+            host=host,
+            database=db)
